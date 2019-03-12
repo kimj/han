ff2421cc493df14e1ec08fddf4007894bccf77af
kimj <jinn.kim@gmail.com>
2019-03-12 19:15:20 -0400

    code

---
+++ app/src/main/java/com/mentalmachines/han/data/repository/GrammarRepository.kt
@@ -0,0 +1,5 @@
+package com.mentalmachines.han.data.repository
+
+import com.mentalmachines.han.data.repository.base.NetworkRepository
+
+class GrammarRepository : NetworkRepository()
---
+++ app/src/main/java/com/mentalmachines/han/ui/grammar/GrammarFragment.kt
@@ -0,0 +1,10 @@
+package com.mentalmachines.han.ui.grammar
+
+import com.mentalmachines.han.ui.base.BaseFragment
+
+class GrammarFragment : BaseFragment(){
+    override val layout: Int
+        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+
+
+}
